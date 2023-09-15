8bf40d31e3526e9c643fbb6037f5b86089e4c357
dtrdic <david@nevercode.io>
2023-09-15 14:55:47 +0200

    dev2

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-//burek6123123d4
+//burek6123123d4d
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
