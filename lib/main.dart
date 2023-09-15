7bb2ee868ef39773986b89f50c75cc52cfe6bc8b
dtrdic <david@nevercode.io>
2023-09-15 14:20:04 +0200

    unos

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-//comment broj2
+//unos
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
